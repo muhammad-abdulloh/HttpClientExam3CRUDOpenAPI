deee2192593c6735f763158a38eda5415487caa4
Clever Coder <92303840+clevercoderr@users.noreply.github.com>
2022-03-03 11:30:04 +0500

    Finished

--- HttpClientExam3/Service/IMovieService.cs
+++ HttpClientExam3/Service/IMovieService.cs
@@ -9,40 +9,14 @@ namespace HttpClientExam3.Service
 {
     internal interface IMovieService
     {
-        /// <summary>
-        /// Get all
-        /// </summary>
-        /// <returns></returns>
         Task<IEnumerable<Movie>> GetAllAsync();
 
-        /// <summary>
-        /// Get
-        /// </summary>
-        /// <param name="id"></param>
-        /// <returns></returns>
         Task<Movie> GetAsync(long id);
 
-        /// <summary>
-        /// Create
-        /// </summary>
-        /// <param name="movie"></param>
-        /// <returns></returns>
         Task<Movie> CreateAsync(Movie movie);
 
-        /// <summary>
-        /// Update
-        /// </summary>
-        /// <param name="id"></param>
-        /// <param name="movie"></param>
-        /// <returns></returns>
         Task<Movie> UpdateAsync(long id, Movie movie);
 
-        /// <summary>
-        /// Delete
-        /// </summary>
-        /// <param name="id"></param>
-        /// <returns></returns>
         Task<bool> DeleteAsync(long id);
-
     }
 }
